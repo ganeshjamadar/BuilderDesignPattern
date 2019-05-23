c9ebf2d54a7d68b5c9426be6d20c471653608e78
ganeshjamadar <ganeshjamadar123@gmail.com>
2019-05-23 21:40:00 +0530

    Final

--- BuilderDesignPattern/Program.cs
+++ BuilderDesignPattern/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace BuilderDesignPattern
 {
+    /// <summary>
+
+    /// MainApp startup class for Structural
+
+    /// Builder Design Pattern.
+
+    /// </summary>
     class Program
     {
+        
+        /// /// <summary>
+
+        /// Entry point into console application.
+
+        /// </summary>
         static void Main(string[] args)
         {
+            // Create director and builders
+
+            Director director = new Director();
+
+            Builder b1 = new ConcreteBuilder1();
+            Builder b2 = new ConcreteBuilder2();
+
+            // Construct two products
+
+            director.Construct(b1);
+            Product p1 = b1.GetResult();
+            p1.Show();
+
+            director.Construct(b2);
+            Product p2 = b2.GetResult();
+            p2.Show();
+
+            // Wait for user
+
+            Console.ReadKey();
         }
     }
 }
